c1a191b01dc760fa1bd0fdf0052cd51dfb80fe7e
Mårten Pålsson <marten.palsson@gmail.com>
2019-07-05 14:24:31 +0200

    Created the basic scenes of the game, all thats left is to connect it all to a canvas

---
+++ HUD.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+signal start_game
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	$MessageLabel.text = "Dodge the\nCreeps!"
+	$MessageLabel.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$StartButton.show()
